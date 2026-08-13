b3bcc0ed2e90a94c56a6130d8d3f0b63448a5e9e
Fabricio Sartor <frssartor@gmail.com>
2026-08-13 13:34:57 -0300

    Pruebas con Docker

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,4 +1,6 @@
 //URL donde corre el backend Express
-export const API_URL = 'http://localhost:7000'
+//export const API_URL = 'http://localhost:7000'
 //export const API_URL = 'http://192.168.10.207:7000'
+//export const API_URL = 'http://192.168.0.59:7000'
+export const API_URL = 'http://192.168.0.50:7000'
 export const KEY_MAPS = "AIzaSyDrghY4eGEqIhsqZsY1KsVKX7SVneu3RTY"
